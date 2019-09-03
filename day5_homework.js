fd7a7b09bf6ab4e4a2534d6b815bcf64fa330190
Suho Kim <suho.kim2@gmail.com>
2019-09-03 19:44:23 +0900

    Create day5_homework.js

---
+++ day5_homework.js
@@ -0,0 +1,53 @@
+var people = [{
+  name: 'Kim Suho',
+  age: 35,
+  job: 'developer'
+}, {
+  name: 'Hong Gildong',
+  age: 40,
+  job: 'thief'
+}, {
+  name: 'Pack Mujin',
+  age: 42,
+  job: 'president'
+}, {
+  name: 'Kent Beck',
+  age: 62,
+  job: 'developer'
+}, {
+  name: 'Martin Fowler',
+  age: 65,
+  job: 'developer'
+}, {
+  name: 'IU',
+  age: 27,
+  job: 'singer'
+}, {
+  name: 'Mun Jaein',
+  age: 63,
+  job: 'president'
+}, {
+  name: 'Park Hyoshin',
+  age: 40,
+  job: 'singer'
+}];
+var numbers = [5, 12, 34, 22, -6, 77, 30, 55, 102, 32, 1155, 324, 1142, 998, 921, 471, 753, 662, 432];
+
+
+var 홀수들 = numbers.xxxxx();
+console.log('홀수들: ', 홀수들);
+
+var 곱하기2한애들 = numbers.xxxxx();
+console.log('곱하기2한애들: ', 곱하기2한애들);
+
+var 곱하기2한애들을숫자순서대로정렬 = numbers.xxxxx().xxxxx();
+console.log('곱하기2한애들을숫자순서대로정렬: ', 곱하기2한애들을숫자순서대로정렬);
+
+var 나이가40이상인사람들 = people.xxxxx();
+console.log('나이가40이상인사람들: ', 나이가40이상인사람들);
+
+var 나이가50이상인사람들의이름 = people.xxxxx().xxxxx();
+console.log('나이가50이상인사람들의이름: ', 나이가50이상인사람들의이름);
+
+var 직업이developer인사람을나이순서대로정렬 = people.xxxxx().xxxxx();
+console.log('직업이developer인사람을나이순서대로정렬: ', 직업이developer인사람을나이순서대로정렬);
